fe886a2242c63ba0c0b93db2741499adbacfd98b
sanjayv2002 <sanjay.murthy.29@gmail.com>
2024-01-27 08:23:32 +0530

    all code i ever wrote

--- src/test/java/com/example/electronics/ElectronicsApplicationTests.java
+++ src/test/java/com/example/electronics/ElectronicsApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.electronics;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class ElectronicsApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("Starting test application");
     }
 
 }
--- src/test/java/com/example/electronics/model/ProductTest.java
+++ src/test/java/com/example/electronics/model/ProductTest.java
@@ -6,6 +6,7 @@ import com.example.electronics.service.ProductService;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,8 +16,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.when;
 
+@Slf4j
 @ExtendWith(MockitoExtension.class)
 public class ProductTest {
 
@@ -30,16 +33,25 @@ public class ProductTest {
 
     @Test
     void testValidProduct(){
-        Product product = TestDataUtils.createTestProductA();
 
-        Set<ConstraintViolation<Product>> violations = validator.validate(product);
-        assertEquals(0, violations.size());
+        try {
+            Product product = TestDataUtils.createTestProductA();
+            log.info("Inside testValidProduct:Creating a test product {}", product.getProductId());
+            Set<ConstraintViolation<Product>> violations = validator.validate(product);
+            assertEquals(0, violations.size());
 
-        when(productRepository.save(product)).thenReturn(product);
-        Product savedProduct = productService.addProduct(product);
-        assertEquals(product, savedProduct);
+            when(productRepository.save(product)).thenReturn(product);
+            Product savedProduct = productService.addProduct(product);
+            assertEquals(product, savedProduct);
+            log.info("Inside testValidProduct:product validation test passed");
+        } catch (Exception e) {
+            log.error("Inside testValidProduct: An {} Exception was thrown", e);
+            fail("An error occured while testing valid product:{} ", e);
+        }
 
     }
+
+
 
 
 }
